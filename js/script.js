1874b8205e654358388b8c2938741e3a26967a6e
Josh Bransford <baxter.bransford@gmail.com>
2026-04-13 13:16:42 -0400

    added mobile navigation for tailwind styles

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ const navLinks = document.querySelector('.navLinks');
 mobileNav.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 });
+
+//Tailwind mobile navigation toggle
+function toggleMenu() {
+  const nav = document.getElementById('navLinks');
+  const isOpen = nav.dataset.open === 'true';
+
+  if(isOpen) {
+    //When function runs while menu is open
+    //removes the open class styles, and restores
+    //closed styling
+    nav.classList.remove('max-h-[700px]', 'opacity-100', 'translate-y-0', 'py-4');
+    nav.classList.add('max-h-0', 'opacity-0', '-translate-y-1.5');
+    nav.dataset.open = 'false';
+  } else {
+    //When function runs while menu is closed
+    //Applys open class styles
+    nav.classList.remove('max-h-0', 'opacity-0', '-translate-y-1.5');
+    nav.classList.add('max-h-[700px]', 'opacity-100', 'translate-y-0', 'py-4');
+    nav.dataset.open = 'true';
+  }
+}
